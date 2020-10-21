e03dfba600631046123a1f5d94a8c200d905ee08
MinJoonAhn <mjsky0041@gmail.com>
2020-10-21 16:18:26 +0900

    기능 코드-1

---
+++ src/main/Situation.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Situation {
+	public int code;
+	public Map<String, Integer> map;
+	
+	public Situation(int code) {
+		this.code = code;
+	}
+	
+	public void put(Map map) {}
+}

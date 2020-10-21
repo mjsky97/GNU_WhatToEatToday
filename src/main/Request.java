7329731a45894f25585087112cdd0a99e6302572
MinJoonAhn <mjsky0041@gmail.com>
2020-10-21 16:08:51 +0900

    새로운 시작을 위한 1보 후퇴

--- src/main/Request.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class Request {
-	private String request;
-	
-	private void set_request(String text) {}
-}
--- src/main/Review.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Review {
-	public String review;
-	public int star_point;
-	
-	public void set_review(String text) {}
-	public void set_star_point(int sp) {}
-}

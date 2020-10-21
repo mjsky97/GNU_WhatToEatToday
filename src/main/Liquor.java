8eb45c8007b868e5bb1acfd320a467d7e4500981
MinJoonAhn <mjsky0041@gmail.com>
2020-10-21 15:37:51 +0900

    기능코드구현 1차

---
+++ src/main/Liquor.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Liquor {
+	public int code;
+	public Map<String, Integer> map;
+	
+	public Liquor(int code) {
+		this.code = code;
+	}
+	
+	public void put(Map map) {}
+}
---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("31231231233213123");
+		
+		
+		
+		
+		
+	}
+
+}
